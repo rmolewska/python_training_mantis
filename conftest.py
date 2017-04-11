100f9f83e479668a4255b206d104f4c08d7953ee
rmolewska <keirekgra@gmail.com>
2017-04-11 22:00:24 +0200

    dodanie testu logowania (test_login)

--- conftest.py
+++ conftest.py
@@ -3,9 +3,7 @@ import pytest
 from fixtures.application import Application
 import json
 import os.path
-import importlib
-import jsonpickle
-from fixtures.db import DbFixture
+
 
 fixture = None
 web_config = None
@@ -27,19 +25,7 @@ def app(request):
     web_config = load_config(request.config.getoption("--target"))["web"]
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config["baseUrl"])
-    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
-
-
-@pytest.fixture(scope="session")
-def db(request):
-    db_config = load_config(request.config.getoption("--target"))["db"]
-    dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"],
-                          password=db_config["password"])
-    def fin():
-        dbfixture.destroy()
-    request.addfinalizer(fin)
-    return dbfixture
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -51,32 +37,10 @@ def stop(request):
     return fixture
 
 
-@pytest.fixture
-def check_ui(request):
-    return request.config.getoption("--check_ui")
-
-
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
-    parser.addoption("--check_ui", action="store_true")
 
 
-def pytest_generate_tests(metafunc):
-    for fixture in metafunc.fixturenames:
-        if fixture.startswith("data_"):
-            test_data = load_from_module(fixture[5:])
-            metafunc.parametrize(fixture, test_data, ids=[str(x) for x in test_data])
-        elif fixture.startswith("json_"):
-            test_data = load_from_json(fixture[5:])
-            metafunc.parametrize(fixture, test_data, ids=[str(x) for x in test_data])
 
-
-def load_from_module(module):
-    return importlib.import_module("data.%s" % module).testdata
-
-
-def load_from_json(file):
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
-        return jsonpickle.decode(f.read())
 
